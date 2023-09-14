e702f66b9f1c2ad99d43dba5ef6a055fda651d9d
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-09-14 16:22:56 +0600

    initial single page rountin

--- app/offer/[id]/page.jsx
+++ app/offer/[id]/page.jsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from "react";
 const OfferDetails = ({ params }) => {
   const [item, setItem] = useState();
 
-  // console.log("this is my params id", params.id);
+  const [status, setStatus] = useState(item?.status);
+
   const id = params.id;
   const fetchSingleOffer = async (id) => {
     const response = await fetch(`http://localhost:3000/api/offers/` + id);
@@ -20,9 +21,10 @@ const OfferDetails = ({ params }) => {
 
   return (
     <section className="container mx-auto px-5 my-20">
-      <div>
-        <div className="border rounded-md shadow-md bg-gray-100 p-2">
-        <Image
+      {/* top identification card */}
+      <div className="flex justify-start gap-5 border rounded-md shadow-md bg-gray-100 p-2">
+        <div className="">
+          <Image
             height={100}
             width={100}
             sizes="100vw"
@@ -30,6 +32,70 @@ const OfferDetails = ({ params }) => {
             alt="card"
             className="w-44 object-cover rounded-md"
           />
+        </div>
+        <div className="flex flex-col justify-center items-start text-left">
+          <div>
+            <h2 className="text-2xl font-semibold text-[#50DBB4]">
+              {item?.title}
+            </h2>
+            <span className="text-sm text-[#50DBB4]">General manager</span>
+          </div>
+          <div className=" my-5">
+            <span className="text-sm text-gray-50 font-bold font-serif shadow-md p-2 bg-yellow-400 rounded-md">
+              Fast Delivery
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* range selector and input */}
+      <div className="grid grid-cols-1 md:grid-cols-3 justify-between items-start gap-y-5 md:gap-5 my-5 ">
+        <div className="col-span-2 shadow-md bg-gray-100 border">
+          <h1 className="text-[#50DBB4] p-5 border-b-2 ">Select Recharge</h1>
+          <div className="m-5 grid grid-cols-1 md:grid-cols-2 gap-y-3 md:gap-3 ">
+            {item?.offers?.map((i) => {
+              return (
+                <div
+                  disabled={i.sold}
+                  key={i}
+                  className="flex justify-between items-center my-2 p-2 bg-white hover:bg-[#50dbb42f] border hover:border-[#50dbb42f] text-gray-400  font-semibold rounded-md duration-700"
+                >
+                  <div className="flex flex-col items-start">
+                    <span className="text-xl">{i?.quantity} Diamond</span>
+                    <span className="text-sm mt-2">BDT {i?.price}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-5">
+          <div className=" shadow-md bg-gray-100 border">
+            <h1 className="text-[#50DBB4] p-5 border-b-2 ">
+              Give Your Account Info
+            </h1>
+            <div className="m-5 grid grid-cols-1 gap-3 justify-center items-center">
+              <label htmlFor="player_id">Player ID</label>
+              <input
+                type="text"
+                name="player_id"
+                id="player_id"
+                placeholder="Enter your player ID"
+                className="border p-2 rounded-md w-full focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className=" shadow-md bg-gray-100 border">
+            <h1 className="text-[#50DBB4] p-5 border-b-2 ">Payment</h1>
+            <div className="m-5 flex justify-between items-center">
+              <span>Total Amount Is : </span>
+              <span> 000 BDT </span>
+            </div>
+            <div className="flex justify-center">
+              <button className=" text-sm text-gray-50 font-bold font-serif shadow-md m-5 p-2 bg-yellow-400 rounded-md w-full">
+                Buy Now
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- app/offer/offercard.jsx
+++ app/offer/offercard.jsx
@@ -7,35 +7,53 @@ import Link from "next/link";
 const OfferCard = ({ offer }) => {
   const [status, setStatus] = useState(offer?.status);
   return (
-    <Link href={`/offer/${offer?._id}`}>
-      {" "}
-      <div className="border shadow-md p-3 w-full md:w-fit h-full bg-gray-100 rounded-md">
-        <div className="overflow-hidden ">
-          <Image
-            height={100}
-            width={100}
-            sizes="100vw"
-            src={offer?.image}
-            alt="card"
-            className="w-full object-cover rounded-md hover:scale-105 transition duration-500 ease-in-out"
-          />
+    <>
+      {offer?.length !== 0 ? (
+        <Link href={`/offer/${offer?._id}`}>
+          <div className="border shadow-md p-3 w-full md:w-fit h-full bg-gray-100 rounded-md">
+            <div className="overflow-hidden ">
+              <Image
+                height={100}
+                width={100}
+                sizes="100vw"
+                src={offer?.image}
+                alt="card"
+                className="w-full object-cover rounded-md hover:scale-105 transition duration-500 ease-in-out"
+              />
+            </div>
+            <div className="pt-2 flex justify-between items-center">
+              <span className="font-semibold text-md text-gray-600">
+                {offer?.title}{" "}
+              </span>
+              {status === "true" ? (
+                <span className="ml-auto bg-[#50DBB4] p-1 rounded-md text-white btn">
+                  In Stock
+                </span>
+              ) : (
+                <span className="ml-auto bg-[#f35353c7] p-1 rounded-md text-white btn">
+                  Sold Out
+                </span>
+              )}
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <div className="py-4 rounded shadow-md w-60 sm:w-80 animate-pulse bg-gray-400">
+          <div className="flex p-4 space-x-4 sm:px-8">
+            <div className="flex-shrink-0 w-16 h-16 rounded-full dark:bg-gray-700"></div>
+            <div className="flex-1 py-2 space-y-4">
+              <div className="w-full h-3 rounded dark:bg-gray-700"></div>
+              <div className="w-5/6 h-3 rounded dark:bg-gray-700"></div>
+            </div>
+          </div>
+          <div className="p-4 space-y-4 sm:px-8">
+            <div className="w-full h-4 rounded dark:bg-gray-700"></div>
+            <div className="w-full h-4 rounded dark:bg-gray-700"></div>
+            <div className="w-3/4 h-4 rounded dark:bg-gray-700"></div>
+          </div>
         </div>
-        <div className="pt-2 flex justify-between items-center">
-          <span className="font-semibold text-md text-gray-600">
-            {offer?.title}{" "}
-          </span>
-          {status === "true" ? (
-            <span className="ml-auto bg-[#50DBB4] p-1 rounded-md text-white btn">
-              In Stock
-            </span>
-          ) : (
-            <span className="ml-auto bg-[#f35353c7] p-1 rounded-md text-white btn">
-              Sold Out
-            </span>
-          )}
-        </div>
-      </div>
-    </Link>
+      )}
+    </>
   );
 };
 
